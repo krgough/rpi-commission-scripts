c9be320846e9adb0222f49fe0645a64529f0d180
Keith Gough <krgough@gmail.com>
2016-05-17 20:58:21 +0000

    updates

---
+++ ip_mailer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:
+
+username="$(cat /home/pi/repositories/audioRepository/audio-notifications/userEmail.txt)"
+ip="$(hostname -I)"
+hostname="$(hostname)"
+
+python3 /home/pi/repositories/audioRepository/audio-notifications/audioSendmail.py -r $username -s "$hostname, $ip" -m "rpi ip address mailer - brought to you by Keith"
