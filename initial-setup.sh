d3cf4eea85d2c7f11988d9117a4268d38d1445a7
Keith Gough <krgough@gmail.com>
2018-06-05 07:02:39 +0100

    updates

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,64 +1,40 @@
 #!/bin/bash
 
-# Setup to configure the rPI
-# Create tars of ssh-config and rpi-commission-scripts (remove old tars first if they exist)
-# 
-# Start the web server to allow rPI to wget 
-# Delete old tars
-# Kill off the web server
+# Initially we cannot clone repos without entering a username/password
+# I don't want to hard code those (or show them in Bash history) so
+# instead we setup the correct hostname on the rPi first then use 
+# local (i.e. laptop)# ssh config to forward our ssh key by setting
+# "forwardAgent yes" for the new rPI hostname.
+# Once hostname is working we can then ssh using that and git clone will
+# work.  This script does the initial setup.
 
-RPI_IP=$1    # rPI IP address
-HN=$2        # New hostname for rPI
-AWS_PORT=$3  # Port number for AWS tunnel
+# 1. Use LED flash to physically id device matches IP address
+# 2. Copy ssh config files (common ssh key, config and authorized_keys)
+# 3. Prompt user to ssh into rPi and clone the rpi-commision repo
 
-WGET_IP="$(ifconfig en0 | grep 'inet ' | awk '{print $2}')"
-WGET_PORT="3000"
-RCS="/home/pi/repositories/rpi-commission-scripts"
+# Run this with an IP address for the rPi as a command line parameter
+IP=$1
 
-if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
-    echo "usage initial-setup.sh <rPi_ip_addr> <hostname> <aws_port>"
+if [ -z $1 ]; then
+    echo "usage initial-setup.sh ipaddress"
     exit 1
 fi
 
-function deleteTarballs {
-  # Delete any old tarballs
-  if [ -e "rpi-commission-scripts.tar" ]; then
-    rm rpi-commission-scripts.tar
-  fi
-}
+echo "*** WARNING: This is the edited untested version"
+echo ""
 
-# Main script starts
+# Copy the SSH config files
+echo "Copying SSH configuration files (stuff we don't want to share in github)..."
+ssh pi@$1 mkdir "/home/pi/.ssh"
+scp ssh-config/config ssh-config/id_rsa ssh-config/authorized_keys pi@$1:/home/pi/.ssh/
 
-# Create the new tarballs, delete old ones first if necessary
-deleteTarballs
-tar -zcf rpi-commission-scripts.tar *
+# Copy the rpi-commission scripts to the device
+ssh pi@$1 mkdir -p "/home/pi/repositories/rpi-commission-scripts"
+scp * pi@$1:/home/pi/repositories/rpi-commission-sctips/
 
-# Start the webserver (for wget)
-#echo python3 -m http.server $WGET_PORT --bind $WGET_IP
-python3 -m http.server $WGET_PORT --bind $WGET_IP > /dev/null 2>&1 &
-
-# SSH to the pi.
-# Make the rpi-commission-scripts directory.
-# Make the .ssh folder and copy ssh-config contenmts there. Public keys and aws login key.
-# Run the rpiSetup script
-
-ssh pi@$RPI_IP /bin/bash << myCommands
-  mkdir -p $RCS;
-  wget -q http://$WGET_IP:$WGET_PORT/rpi-commission-scripts.tar -O $RCS/rpi-commission-scripts.tar
-  tar -xf $RCS/rpi-commission-scripts.tar -C $RCS
-  rm $RCS/rpi-commission-scripts.tar
-  
-  # SSH config is in the tarball so we can create a .ssh and copy the files there.
-  mkdir -p /home/pi/.ssh
-  cp $RCS/ssh-config/* /home/pi/.ssh
-
-  # Run the rpi-commission.sh script
-  sudo $RCS/commissionScript.sh $HN $AWS_PORT 
-
-myCommands
-
-# Kill the web server and delete the old tarballs
-pkill -f "Python -m http.server"
-wait $! 2>/dev/null
-deleteTarballs
-echo "All done."
+# Prompt user to ssh into the device directly and do the rest
+echo
+echo "*************************"
+echo "All done.  ssh onto device and run ./commissionScript.sh"
+echo "ssh pi@$1"
+echo "./commissionScript.sh"
