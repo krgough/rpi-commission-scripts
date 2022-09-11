98a682569994dace0d4d29b1dd09802959c4d475
krgough <krgough@gmail.com>
2022-09-11 17:15:35 +0100

    ssh restart added to disable_ssh_pwd_login

--- disable_ssh_pwd_login.sh
+++ disable_ssh_pwd_login.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+if [ "$(id -u)" -ne "0" ]; then
+    echo "This script must be executed with root privileges"
+    exit 1
+fi
+
 ssh_cfg_path="/etc/ssh/sshd_config"
 echo "Setting 'Password Authentication no' in $ssh_cfg_path"
 echo "PasswordAuthentication no" | sudo tee -a $ssh_cfg_path > /dev/null
+
+systemctl restart ssh
