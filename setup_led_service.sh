fa81157b66e9a975db64ab8ccbba138f12fec4e8
Keith Gough <krgough@gmail.com>
2022-12-10 12:25:15 +0000

    Create setup_led_service.sh

---
+++ setup_led_service.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+# Setup systemd service to run the led indicator service
+echo "Setting up led_widi_indicator.service in systemd..."
+sudo cp ./led_wifi_indicator.service /etc/systemd/system/
+sudo systemctl enable led_wifi_indicator.service
+sudo systemctl start led_wifi_indicator.service
+sudo systemctl status led_wifi_indicator.service
